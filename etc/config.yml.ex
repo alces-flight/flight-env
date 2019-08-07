8482cdc6550dca49719353bc30cdfc4534aa5478
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-08-07 17:52:46 +0100

    Update to config files

---
+++ etc/config.yml.ex
@@ -0,0 +1,5 @@
+---
+# Path to global/system-wide depots
+#global_depot_path: /opt/flight/var/lib/env
+# Path to global/system-wide build cache
+#global_build_cache_path: /opt/flight/var/cache/env/build
