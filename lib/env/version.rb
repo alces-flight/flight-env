bb0c1b431dff96f9f11455e02331b71be16ec1c2
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-09-11 17:28:52 +0100

    Bump version

--- lib/env/version.rb
+++ lib/env/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/alces-flight/flight-env
 # ==============================================================================
 module Env
-  VERSION = '1.1.1'
+  VERSION = '1.1.2'
 end
